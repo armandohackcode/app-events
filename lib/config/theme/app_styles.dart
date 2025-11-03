45672d00bf3bcd119562a43d03f2915938677959
Juan Agustin Apaza Guzman <japaza.pasantedlabs@datec.com.bo>
2025-11-03 15:54:23 -0400

    Agregar soporte para UI/UX en el sistema de recursos y estilos

--- lib/config/theme/app_styles.dart
+++ lib/config/theme/app_styles.dart
@@ -15,6 +15,7 @@ class AppStyles {
   static const Color colorBaseRed = Color(0xFFEA4535);
   static const Color colorBaseGreen = Color(0xFF36BD57);
   static const Color colorBaseYellow = Color(0xFFFFBB00);
+  static const Color colorBasePurple = Color(0xFFAB47BC); // UI/UX
   static const Color colorNavbar = Color.fromARGB(255, 47, 47, 47);
   static const Color colorAppbar = Colors.black;
 }
